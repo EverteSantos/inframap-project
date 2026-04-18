ca714f4ecac02d204277ffd0ccf8cc43d7a8a57b
Everte Santos <evertesantos214@gmail.com>
2026-04-18 15:09:56 -0300

    Criando routes

---
+++ app/routes/tipo_ativos_route.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database.conection import get_db
+from app.schemas.tipo_ativo_schema import TipoAtivoCreate, TipoAtivoResponse
+from app.models.tipo_ativo import TipoAtivo
+
+router = APIRouter(prefix='/tipo_ativo', tags = ['tipo_ativos'])
+
+@router.post('/', response_model = TipoAtivoResponse)
+def definir_tipo_ativo(tipo_ativo: TipoAtivoCreate, db: Session = Depends(get_db)):
+    
+    tipo_ativo_db = db.query(TipoAtivo).filter(TipoAtivo.nome == tipo_ativo.nome).first()
+
+    if tipo_ativo_db:
+        raise HTTPException(status_code=400, detail="Tipo de ativo já cadastrado.")
+
+    novo_tipo_ativo = TipoAtivo(
+        nome = tipo_ativo.nome.strip().title()
+    )
+
+    db.add(novo_tipo_ativo)
+    db.commit()
+    db.refresh(novo_tipo_ativo)
+
+    return novo_tipo_ativo
+
+@router.get('/', response_model = list[TipoAtivoResponse])
+def listar_tipo_ativos(db: Session = Depends(get_db)):
+
+    lista_tipo_ativos = db.query(TipoAtivo).all()
+
+
+    return lista_tipo_ativos
+
+@router.get('/{tipoa_ativo_id}', response_model=TipoAtivoResponse)
+def busca_por_id(tipoa_ativo_id: int, db: Session = Depends(get_db)):
+
+    busca_ativo_id = db.query(TipoAtivo).filter(TipoAtivo.id == tipoa_ativo_id).first()
+
+    if not busca_ativo_id:
+        raise HTTPException(status_code=404, detail='ID não existe.')
+
+    return busca_ativo_id
+
+@router.put('/', response_model=TipoAtivoResponse)
+def 
